6e2d8e03b7122bff445a29a385594e22e3b82816
PeripheralVisionPD2 <jaypoopys@gmail.com>
2024-07-02 13:16:12 -0400

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   .vs/LanFileSenRec/DesignTimeBuild/.dtbcache.v2
    	new file:   .vs/LanFileSenRec/FileContentIndex/2da686cd-f85d-46ed-ae66-ae5806e254ed.vsidx
    	new file:   .vs/LanFileSenRec/FileContentIndex/2dc1399b-dcf7-4d23-8979-0fcbd9c2634e.vsidx
    	new file:   .vs/LanFileSenRec/FileContentIndex/39811495-44c6-4467-b877-ec65cb861acf.vsidx
    	new file:   .vs/LanFileSenRec/v17/.futdcache.v2
    	new file:   .vs/LanFileSenRec/v17/.suo
    	new file:   .vs/LanFileSenRec/v17/DocumentLayout.json
    	new file:   .vs/ProjectEvaluation/lanfilesenrec.metadata.v8.bin
    	new file:   .vs/ProjectEvaluation/lanfilesenrec.projects.v8.bin
    	new file:   .vs/ProjectEvaluation/lanfilesenrec.strings.v8.bin
    	new file:   LanFileClient/LanFileClient.csproj
    	new file:   LanFileClient/Program.cs
    	new file:   LanFileClient/bin/Debug/net8.0/0.0.0.0
    	new file:   LanFileClient/bin/Debug/net8.0/LanFileClient.deps.json
    	new file:   LanFileClient/bin/Debug/net8.0/LanFileClient.dll
    	new file:   LanFileClient/bin/Debug/net8.0/LanFileClient.exe
    	new file:   LanFileClient/bin/Debug/net8.0/LanFileClient.pdb
    	new file:   LanFileClient/bin/Debug/net8.0/LanFileClient.runtimeconfig.json
    	new file:   LanFileClient/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs
    	new file:   LanFileClient/obj/Debug/net8.0/LanFileClient.AssemblyInfo.cs
    	new file:   LanFileClient/obj/Debug/net8.0/LanFileClient.AssemblyInfoInputs.cache
    	new file:   LanFileClient/obj/Debug/net8.0/LanFileClient.GeneratedMSBuildEditorConfig.editorconfig
    	new file:   LanFileClient/obj/Debug/net8.0/LanFileClient.GlobalUsings.g.cs
    	new file:   LanFileClient/obj/Debug/net8.0/LanFileClient.assets.cache
    	new file:   LanFileClient/obj/Debug/net8.0/LanFileClient.csproj.BuildWithSkipAnalyzers
    	new file:   LanFileClient/obj/Debug/net8.0/LanFileClient.csproj.CoreCompileInputs.cache
    	new file:   LanFileClient/obj/Debug/net8.0/LanFileClient.csproj.FileListAbsolute.txt
    	new file:   LanFileClient/obj/Debug/net8.0/LanFileClient.dll
    	new file:   LanFileClient/obj/Debug/net8.0/LanFileClient.genruntimeconfig.cache
    	new file:   LanFileClient/obj/Debug/net8.0/LanFileClient.pdb
    	new file:   LanFileClient/obj/Debug/net8.0/apphost.exe
    	new file:   LanFileClient/obj/Debug/net8.0/ref/LanFileClient.dll
    	new file:   LanFileClient/obj/Debug/net8.0/refint/LanFileClient.dll
    	new file:   LanFileClient/obj/LanFileClient.csproj.nuget.dgspec.json
    	new file:   LanFileClient/obj/LanFileClient.csproj.nuget.g.props
    	new file:   LanFileClient/obj/LanFileClient.csproj.nuget.g.targets
    	new file:   LanFileClient/obj/project.assets.json
    	new file:   LanFileClient/obj/project.nuget.cache
    	new file:   LanFileServer/.vs/LanFileServer/DesignTimeBuild/.dtbcache.v2
    	new file:   LanFileServer/.vs/LanFileServer/v17/.futdcache.v2
    	new file:   LanFileServer/.vs/LanFileServer/v17/.suo
    	new file:   LanFileServer/LanFileServer.csproj
    	new file:   LanFileServer/Program.cs
    	new file:   LanFileServer/bin/Debug/net8.0/LanFileServer.deps.json
    	new file:   LanFileServer/bin/Debug/net8.0/LanFileServer.dll
    	new file:   LanFileServer/bin/Debug/net8.0/LanFileServer.exe
    	new file:   LanFileServer/bin/Debug/net8.0/LanFileServer.pdb
    	new file:   LanFileServer/bin/Debug/net8.0/LanFileServer.runtimeconfig.json
    	new file:   LanFileServer/bin/Debug/net8.0/file.zip
    	new file:   LanFileServer/bin/Debug/net8.0/net8.0.zip

---
+++ LanFileClient/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Sockets;
+using System.Text;
+
+var client = new TcpClient();
+Console.WriteLine("/* LanFileSenRec - Client");
+Console.WriteLine("   domer/PeripheralVisionPD2");
+Console.WriteLine("   7 - 2 - 2024 */");
+PORT_ENTER:
+Console.Write("enter port number: ");
+var port = Int32.Parse(Console.ReadLine());
+if (port > 99999)
+{
+    Console.WriteLine("invalid port");
+    Thread.Sleep(2000);
+    Console.Clear();
+    goto PORT_ENTER;
+}
+Console.Write("enter local ip of server: ");
+var ip = Console.ReadLine();
+List<string> sent = new List<string>();
+foreach (string filename in Directory.GetFiles(@"C:\Users\Public\Documents\LFSR\Send"))
+{
+    byte[] magic = new byte[1];
+    magic[0] = (byte)0x0;
+    if (sent.Contains(filename))
+        break;
+    sent.Add(filename);
+    client = new TcpClient();
+    client.Connect(ip, port);
+
+    byte[] _file = File.ReadAllBytes(filename);
+    byte[] _size = BitConverter.GetBytes(_file.Length);
+    Console.WriteLine($"size of {filename} " + BitConverter.ToInt32(_size) + "bytes");
+
+    if (client.Client.Send(_size) == 0)
+    {
+        Console.WriteLine("error sending file size to server");
+        Console.ReadLine();
+        return;
+    }
+
+    client.Client.Receive(magic);
+    if (magic[0] != (byte)0x1)
+        return;
+    Console.WriteLine("[server reply]");
+    client.Client.Disconnect(false);
+    client = new TcpClient();
+    client.Connect(ip, port);
+
+    string _filename = filename.Replace("C:\\Users\\Public\\Documents\\LFSR\\Send\\", "");
+    Console.WriteLine($"sending {_filename} to server");
+    if (client.Client.Send(Encoding.UTF8.GetBytes(_filename)) == 0)
+    {
+        Console.WriteLine("error sending file name to server");
+        Console.ReadLine();
+        return;
+    }
+    client.Client.Receive(magic);
+    if (magic[0] != (byte)0x1)
+        return;
+    Console.WriteLine("[server reply]");
+    client.Client.Disconnect(false);
+    client = new TcpClient();
+    client.Connect(ip, port);
+
+    if (client.Client.Send(_file) == 0)
+        Console.WriteLine("error sending file");
+    client.Client.Receive(magic);
+    Console.WriteLine("[server reply]");
+    if (magic[0] != (byte)0x1)
+        return;
+    client.Client.Disconnect(false);
+}
+Console.WriteLine("done. press enter to exit");
+Console.ReadLine();
+return;
---
+++ LanFileServer/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+public class ServerApp
+{
+    private static string SecureRandomString(int length)
+    {
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            var bytes = new byte[length];
+            rng.GetBytes(bytes);
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(bytes.Select(b => chars[b % chars.Length]).ToArray());
+        }
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("/* LanFileSenRec - Server");
+        Console.WriteLine("   domer/PeripheralVisionPD2");
+        Console.WriteLine("   7 - 2 - 2024 */");
+        if (!Directory.Exists(@"C:\Users\Public\Documents\LFSR\Received"))
+            Directory.CreateDirectory(@"C:\Users\Public\Documents\LFSR\Received");
+        try
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+        PORT_ENTER:
+            Console.Write("enter port number: ");
+            var port = Int32.Parse(Console.ReadLine());
+            if (port > 99999)
+            {
+                Console.WriteLine("invalid port");
+                Thread.Sleep(2000);
+                Console.Clear();
+                goto PORT_ENTER;
+            }
+            TcpListener listener = new TcpListener(ip, port);
+            listener.Start();
+            Console.WriteLine("server is listening on " + listener.LocalEndpoint);
+
+            int tracker = 0;
+            int size = 0;
+            string filename = "";
+            byte[] _sdata = new byte[1024];
+            byte[] _ndata = new byte[1024];
+            byte[] awaiting = new byte[1];
+            awaiting[0] = (byte)0x1;
+
+            while (true)
+            {
+                Socket client = listener.AcceptSocket();
+                Console.WriteLine("[packet accepted]");
+
+                var childSocketThread = new Thread(() =>
+                {
+                    switch (tracker)
+                    {
+                        case 0:
+                            Console.WriteLine("reading size...");
+                            client.Receive(_sdata);
+                            size = BitConverter.ToInt32(_sdata);
+                            Console.WriteLine(size + " bytes");
+                            tracker++;
+                            client.Send(awaiting);
+                            client.Close();
+                            return;
+
+                        case 1:
+                            Console.WriteLine("reading filename...");
+                            client.Receive(_ndata);
+                            filename = Encoding.UTF8.GetString(_ndata).Replace("\0", "");
+                            Console.WriteLine(filename);
+                            tracker++;
+                            client.Send(awaiting);
+                            client.Close();
+                            return;
+
+                        case 2:
+                            Console.WriteLine($"writing C:\\Users\\Public\\Documents\\LFSR\\Received\\{filename} bytes...");
+                            byte[] _rdata = new byte[size];
+                            client.Receive(_rdata);
+                            File.WriteAllBytes($"C:\\Users\\Public\\Documents\\LFSR\\Received\\{filename}", _rdata);
+                            Console.WriteLine($"done writing {filename}");
+                            tracker = 0;
+                            filename = "";
+                            client.Send(awaiting);
+                            client.Close();
+                            return;
+                    }
+                });
+
+                childSocketThread.Start();
+            }
+
+            listener.Stop();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("error: " + e.StackTrace);
+            Console.ReadLine();
+        }
+    }
+}
